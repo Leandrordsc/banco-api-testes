3c77a429e71c5a28cb52bf999799d073a7e39702
Leandrordsc <leandrocosta@brq.com>
2026-06-20 18:53:42 -0300

    Adicionando o hook de beforeEach para captura do token

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -6,39 +6,43 @@ require ('dotenv').config()
 
 describe ('Transferências', () => {
         describe ('POS/tranferencias', () => {
-            it('Deve retornar sucesso com 201 quando o valor for igual ou acima de R$10,00', async() => { 
-                const token = await obterToken('julio.lima','123456')
-                const resposta = await request(process.env.BASE_URL)
-                    .post('/transferencias')
-                    .set('Content-Type', 'application/json')
-                    .set('Authorization',`Bearer ${token}` )
-                    .send({
-                         contaOrigem: 1,
-                         contaDestino: 2,
-                         valor: 10,
-                         token: ""
 
-                    })
+              let token
+              beforeEach(async() => {
+            token = await obterToken('julio.lima','123456')
+    })
 
-                    expect(resposta.status).to.equal(201);                 
+                it('Deve retornar sucesso com 201 quando o valor for igual ou acima de R$10,00', async() => { 
+                        const resposta = await request(process.env.BASE_URL)
+                        .post('/transferencias')
+                        .set('Content-Type', 'application/json')
+                        .set('Authorization',`Bearer ${token}` )
+                        .send({
+                            contaOrigem: 1,
+                            contaDestino: 2,
+                            valor: 10,
+                            token: ""
+
+                        })
+
+                        expect(resposta.status).to.equal(201);                 
+                })
+
+                it('Deve retornar falha com 422 quando o valor for baixo de R$10,00',async() => {
+                    const resposta = await request(process.env.BASE_URL)
+                        .post('/transferencias')
+                        .set('Content-Type', 'application/json')
+                        .set('Authorization',`Bearer ${token}` )
+                        .send({
+                            contaOrigem: 1,
+                            contaDestino: 2,
+                            valor: 9.99,
+                            token: ""
+
+                        })
+
+                        expect(resposta.status).to.equal(422);
+                })
             })
 
-             it('Deve retornar falha com 422 quando o valor for baixo de R$10,00',async() => {
-                const token = await obterToken ('julio.lima','123456')
-                const resposta = await request(process.env.BASE_URL)
-                    .post('/transferencias')
-                    .set('Content-Type', 'application/json')
-                    .set('Authorization',`Bearer ${token}` )
-                    .send({
-                         contaOrigem: 1,
-                         contaDestino: 2,
-                         valor: 9.99,
-                         token: ""
-
-                    })
-
-                    expect(resposta.status).to.equal(422);
-            })
-        })
-
-})
+    })
